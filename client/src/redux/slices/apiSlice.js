956378f53532fbd4c6cf700b630776fb50b59609
Billy Chen <billy61307aaa@gmail.com>
2024-11-09 21:42:17 +0800

    update taSkRoutes.js client\apiSlice.js

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -2,7 +2,18 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const API_URI = import.meta.env.VITE_API_URI;
 
-const baseQuery = fetchBaseQuery({ baseUrl: API_URI, credentials: "include" });
+const baseQuery = fetchBaseQuery({
+  baseUrl: API_URI,
+  credentials: "include",
+  prepareHeaders: (headers) => {
+    headers.set("Access-Control-Allow-Credentials", "true");
+    const token = localStorage.getItem("token");
+    if (token) {
+      headers.set("Authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
 
 export const apiSlice = createApi({
   baseQuery,
