bbc60f8cb40adef127bf042b039d108a30ee9cea
Billy Chen <billy61307aaa@gmail.com>
2024-11-10 00:32:02 +0800

    add connectDB.js

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,15 +1,4 @@
 import jwt from "jsonwebtoken";
-import mongoose from "mongoose";
-
-const dbConnection = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-
-    console.log("DB connection established");
-  } catch (error) {
-    console.log("DB Error: " + error);
-  }
-};
 
 const createJWT = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
@@ -27,4 +16,4 @@ const createJWT = (res, userId) => {
   return token;
 };
 
-export { dbConnection, createJWT };
+export default createJWT;
